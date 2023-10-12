cf457ecd958f63b06a70636ccd255e4f69e9f915
dfrgr <dfrgros@gmail.com>
2023-10-12 14:01:18 +0200

    pom.xml artifactID

--- src/test/java/robot/MouvementFixture.java
+++ src/test/java/robot/MouvementFixture.java
@@ -11,4 +11,32 @@ import java.util.Random;
 public class MouvementFixture {
     private Robot wallE = new Robot(0 , new Battery(), new RoadBookCalculator());
 
+    public String resultatMouvement(String mouvement) throws InaccessibleCoordinate, UnlandedRobotException, InsufficientChargeException, LandSensorDefaillance {
+
+       wallE.land(new Coordinates(2, 2), new LandSensor(new Random()));
+
+        String result = "";
+
+       switch(mouvement) {
+           case "se déplace en avant":
+               wallE.moveForward();
+               if(wallE.getYposition()==1) result="avance d'une case vers le nord";
+               break;
+           case "se déplace en arrière":
+               wallE.moveBackward();
+               if(wallE.getYposition()==3) result="avance d'une case vers le sud";
+               break;
+           case "tourne dans le sens":
+               if(wallE.getDirection() == Direction.EAST) result="est orienté vers l'est";
+               break;
+           case "est orienté vers l'est":
+               wallE.turnLeft();
+               if(wallE.getDirection() == Direction.WEST) result="est orienté vers l'ouest";
+               break;
+           default:
+               result = "KO";
+           break;
+       }
+        return result;
+    }
 }
